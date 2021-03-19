6b499d98e5da7292adf9a8b69030d1774a75915d
Gigih.Suryaman <Gigih.Suryaman@21A000119-NB.SINARMASMSIGLIFE.CO.ID>
2021-03-19 11:09:25 +0700

    update clearkey

--- src/main/java/com/app/utils/VegaCustomResourceLoader.java
+++ src/main/java/com/app/utils/VegaCustomResourceLoader.java
@@ -51,6 +51,7 @@ import org.springframework.stereotype.Component;
 
 import com.app.model.DetailWithdraw;
 import com.app.model.LstHistActivityWS;
+import com.app.model.LstUserSimultaneous;
 import com.app.model.Provinsi;
 import com.app.model.User;
 import com.app.services.VegaServices;
@@ -120,11 +121,13 @@ public class VegaCustomResourceLoader implements ResourceLoaderAware {
 	}
 
 	public Boolean validateCredential(String username, String key) {
+		LstUserSimultaneous dataUser = services.selectDataLstUserSimultaneous(username);
+		
 		User paramSelectDecryptPassword = new User();
-		paramSelectDecryptPassword.setKey(key);
+		paramSelectDecryptPassword.setKey(dataUser.getPASSWORD());
 		User keyDecrypt = services.decryptPassword(paramSelectDecryptPassword);
 
-		if (username.equals(keyDecrypt.getKey())) {
+		if (key.equals(keyDecrypt.getKey())) {
 			return true;
 		} else {
 			return false;

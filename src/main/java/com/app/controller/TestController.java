d77bab3833d64bb78bc3693d43fbd72962039819
Gigih.Suryaman <Gigih.Suryaman@21A000119-NB.SINARMASMSIGLIFE.CO.ID>
2021-04-06 11:31:03 +0700

    add checkotp

--- src/main/java/com/app/controller/TestController.java
+++ src/main/java/com/app/controller/TestController.java
@@ -3,6 +3,7 @@ package com.app.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.app.services.VegaServices;
@@ -14,12 +15,18 @@ public class TestController {
 
 	@Autowired
 	private VegaServices vegaServices;
-	
-	@ApiOperation(value="Service untu melakukan test memastikan semua aplikasi berhubungan database jalan ")
+
 	@RequestMapping(value = "/testhit", method = RequestMethod.GET)
 	public String testHit() {
 		String printHit = vegaServices.selectEncrypt("test");
 		return printHit;
+	}
+	
+	@RequestMapping(value = "/checkotp", method = RequestMethod.GET)
+	public String kodeOtp(@RequestParam String no_hp, @RequestParam Integer jenis_id, @RequestParam Integer menu_id) {
+		String code = vegaServices.selectCheckOTP(no_hp, jenis_id, menu_id);
+
+		return code;
 	}
 
 }

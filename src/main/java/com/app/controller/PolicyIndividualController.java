f92568a6757c46679bca2b5f1becab3a4f255348
Patar.Tambunan <Patar.Tambunan@Patarlaptop.SINARMASMSIGLIFE.CO.ID>
2021-07-12 12:41:40 +0700

    go

--- src/main/java/com/app/controller/PolicyIndividualController.java
+++ src/main/java/com/app/controller/PolicyIndividualController.java
@@ -840,7 +840,7 @@ public class PolicyIndividualController {
 					message = "Successfully get penerima manfaat details";
 				} else {
 					error = true;
-					message = "Policy is not active";
+					message = "Data penerima manfaat kosong";
 					resultErr = "Data penerima manfaat kosong";
 					logger.error(
 							"Path: " + request.getServletPath() + " Username: " + username + " Error: " + resultErr);

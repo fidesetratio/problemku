18a758c9ca9032ff2d44c7fddfdcd75e16b58e04
Patar.Tambunan <Patar.Tambunan@Patarlaptop.SINARMASMSIGLIFE.CO.ID>
2021-08-10 10:34:33 +0700

    modified little financial transaction Financial

--- src/main/java/com/app/controller/FinancialTransactionController.java
+++ src/main/java/com/app/controller/FinancialTransactionController.java
@@ -6920,32 +6920,38 @@ public class FinancialTransactionController {
 
 					String tgl_awal;
 					String tgl_akhir;
+					is_tgl_awal_submitted = false;
+					
 					
 					if(lspd_id==13) {
+						
 						is_tgl_awal_submitted = true;
 						Endorse endorse = new Endorse();
 						endorse.setReg_spaj(reg_spaj);
 						endorse = services.selectGetPremiumHolidayDate(reg_spaj);
-						
+					     
+						if(endorse != null) {
 						tgl_awal = endorse.getTgl_awal();
 						tgl_akhir = endorse.getTgl_akhir();
-						
 						data.put("tgl_awal", tgl_awal);
 						data.put("tgl_akhir", tgl_akhir != null ? tgl_akhir : null);
+						}
+						
+						
 					} else if (lspd_id==99){
+					
 						Endorse endorse = new Endorse();
 						endorse.setReg_spaj(reg_spaj);
 						endorse = services.selectGetPremiumHolidayDate(reg_spaj);
 						
+						if(endorse != null) {
 						tgl_awal = endorse.getTgl_awal();
 						tgl_akhir = endorse.getTgl_akhir();
 						
 						data.put("tgl_awal", tgl_awal);
 						data.put("tgl_akhir", tgl_akhir != null ? tgl_akhir : null);
 						is_tgl_awal_submitted = true;
-					}
-					else {
-						is_tgl_awal_submitted = false;
+						};
 					}
 					
 					data.put("is_tgl_awal_submitted", is_tgl_awal_submitted);
